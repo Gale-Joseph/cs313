3094bf00b3b7daafb1b7dd4a5640fe19474d846c
Joseph Gale <gal16026@byui.edu>
2020-01-13 15:40:53 -0600

    week 2 ta upload

--- week2/ta/week2.js
+++ week2/ta/week2.js
@@ -1,3 +1,7 @@
+/*The following commented out code changes the color of
+ * the paragraph with id=pColor to the color inputted by 
+ * user in textarea if textarea text is a valid color */
+
 //function changeColor() {
 //    var color = document.getElementById("textarea").value;
 //    document.getElementById("pColor").style.color = color;
@@ -5,6 +9,11 @@
 //}
 
 
+/* The following function uses jQuery to change the color
+ * of the textarea background to the color typed by the 
+ * user, if the color is valid 
+ * The seond function toggles div3 in and out using 
+ * JQuery FadeToggle*/
 
 $(document).ready(function () {
 
@@ -14,7 +23,7 @@ $(document).ready(function () {
         $("textarea").css("background-color", color);
     });
 
-    //use class 
+   
     $("input.buttonToggle").click(function () {        
             console.log("Using FadeToggle");
             $("#div3").fadeToggle(3000);
