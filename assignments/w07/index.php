a93fbaf5efdd56abf6420b3b849cddbe74dee9e5
Joseph Gale <gal16026@byui.edu>
2020-02-17 08:48:48 -0600

    first w07 upload

---
+++ assignments/w07/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>Sign Up Page</h1>
+<form action='controller.php' method='post'>
+<label for="username">Username</label>  
+    <input type  = "text" name = "username" id = "username">
+<label for ="password">Password</label>
+    <input type ="text" name = "password" id = "password">
+    <button type = "submit" name = "submit" id = "submit">Submit</submit>
+</form>
+    
+</body>
+</html>
